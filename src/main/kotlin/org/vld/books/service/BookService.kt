c0aed3e96c27f87be2e130d6c7a3a95cbe7e81b4
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-12-23 10:54:30 +0100

    feat: add BookService

---
+++ src/main/kotlin/org/vld/books/service/BookService.kt
@@ -0,0 +1,25 @@
+package org.vld.books.service
+
+import org.vld.books.domain.Book
+import org.vld.books.repository.BookRepository
+
+interface BookService {
+    fun findAllBooks(): List<Book>
+    fun findBookById(id: String): Book?
+    fun createBook(book: Book): Book
+    fun updateBook(book: Book): Book
+    fun deleteBookById(id: String): Boolean
+}
+
+class BookServiceImpl(private val bookRepository: BookRepository) : BookService {
+
+    override fun findAllBooks(): List<Book> = bookRepository.findAll()
+
+    override fun findBookById(id: String): Book? = bookRepository.findById(id)
+
+    override fun createBook(book: Book): Book = bookRepository.save(book)
+
+    override fun updateBook(book: Book): Book = bookRepository.save(book)
+
+    override fun deleteBookById(id: String): Boolean = bookRepository.deleteById(id)
+}
