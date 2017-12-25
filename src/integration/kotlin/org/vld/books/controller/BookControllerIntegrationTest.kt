796ca8f1d0f9b7bb8506432c347fe4c143e5cc90
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-12-25 09:09:05 +0100

    test: add GET /books test

--- src/integration/kotlin/org/vld/books/controller/BookControllerIntegrationTest.kt
+++ src/integration/kotlin/org/vld/books/controller/BookControllerIntegrationTest.kt
@@ -1,17 +1,21 @@
 package org.vld.books.controller
 
+import com.jayway.jsonpath.JsonPath
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import org.vld.books.config.BookConfig
@@ -33,8 +37,10 @@ class BookControllerIntegrationTest {
     }
 
     @Test
-    fun aaa() {
-        mockMvc.perform(get("/books")).andDo(print())
+    fun `Given a list of Books when find all Books then return a list of Books`() {
+        val mvcResult: MvcResult = mockMvc.perform(get("/books").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk()).andReturn()
+        val books = JsonPath.parse(mvcResult.response.contentAsString)
+        assertThat(books.read("$") as List<String>).isNotEmpty()
     }
-
 }
