f4432231ca4e65c730aac337215e33d97c825c4b
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-12-25 10:25:48 +0100

    feat: add CorrelationIdInterceptor

--- src/main/kotlin/org/vld/books/config/BookConfig.kt
+++ src/main/kotlin/org/vld/books/config/BookConfig.kt
@@ -3,13 +3,17 @@ package org.vld.books.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.vld.books.domain.Book
+import org.vld.books.interceptor.CorrelationIdInterceptor
 
 @Configuration
 @ComponentScan(basePackages = ["org.vld.books"])
 @EnableWebMvc
-open class BookConfig {
+open class BookConfig : WebMvcConfigurer {
 
     @Bean
     open fun books(): MutableList<Book> = mutableListOf(
@@ -17,4 +21,11 @@ open class BookConfig {
             Book("2", "Pro Spring 5"),
             Book("3", "Spring 5 Recipes")
     )
+
+    @Bean
+    open fun correlationIdInterceptor(): HandlerInterceptor = CorrelationIdInterceptor()
+
+    override fun addInterceptors(registry: InterceptorRegistry?) {
+        registry?.addInterceptor(correlationIdInterceptor())
+    }
 }
