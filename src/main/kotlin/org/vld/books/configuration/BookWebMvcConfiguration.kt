5bd07c560118b9398357b3ae6d1ec4bab5c48fe8
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-12-27 21:18:58 +0100

    refactor: add BookRepositoryConfiguration

--- src/main/kotlin/org/vld/books/configuration/BookWebMvcConfiguration.kt
+++ src/main/kotlin/org/vld/books/configuration/BookWebMvcConfiguration.kt
@@ -7,20 +7,12 @@ import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.vld.books.domain.Book
 import org.vld.books.interceptor.CorrelationIdInterceptor
 
 @Configuration
 @ComponentScan(basePackages = ["org.vld.books"])
 @EnableWebMvc
 open class BookWebMvcConfiguration : WebMvcConfigurer {
-
-    @Bean
-    open fun books(): MutableList<Book> = mutableListOf(
-            Book("1", "Spring REST"),
-            Book("2", "Pro Spring 5"),
-            Book("3", "Spring 5 Recipes")
-    )
 
     @Bean
     open fun correlationIdInterceptor(): HandlerInterceptor = CorrelationIdInterceptor()
